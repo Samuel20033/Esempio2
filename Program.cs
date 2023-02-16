16156c2716fd85dca048f7641f644386b301fc59
Samuel20033 <94487025+Samuel20033@users.noreply.github.com>
2023-02-16 17:13:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Hello, World!!");
+Console.WriteLine("Ciao a tutti");
+Console.WriteLine("Hello, World2");
+Console.WriteLine("Pippo");
+Console.WriteLine("Pippo");
+Console.WriteLine("Pippo");
+Console.WriteLine("Prova pull request");
+Console.WriteLine("Test Gabriele");
+Console.WriteLine("Test Anna");
+
+
+
